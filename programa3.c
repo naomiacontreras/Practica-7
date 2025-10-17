797efb6ec842106f0c5dee38bb069fcc1af968bd
naomiacontreras <naomi.miranda.contreras@gmail.com>
2025-10-17 14:19:54 -0600

    programa3.c

--- programa3.c
+++ programa3.c
@@ -5,8 +5,8 @@ int num;
 printf("Ingrese un número:\n");
 scanf("%d",&num);
 if ( num%2 == 0 )
-printf("El número %d es par.\n",num);
+  printf("El número %d es par.\n",num);
 else
-printf("El número %d es impar.\n",num);
+  printf("El número %d es impar.\n",num);
 return 0;
 }
